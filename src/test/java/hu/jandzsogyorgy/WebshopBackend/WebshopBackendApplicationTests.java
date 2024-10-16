d15cda1650208b378f7acd38bd82dc4cfd80f52f
gyoergy.jandzso@t-systems.com <gyoergy.jandzso@t-systems.com>
2024-10-16 15:37:55 +0200

    Init

---
+++ src/test/java/hu/jandzsogyorgy/WebshopBackend/WebshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.jandzsogyorgy.WebshopBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
